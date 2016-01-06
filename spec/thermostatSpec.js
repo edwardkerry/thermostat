000bf9937820c76adfd23d6e1169a74063fc1a83
Alan Gabbianelli <alan.gabbianelli@gmail.com>
2016-01-06 13:17:05 +0000

    Adds MIN_TEMP

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -6,23 +6,43 @@ describe('Thermostat', function() {
     thermostat = new Thermostat();
   });
 
-  describe('#temp', function(){
-    it('should have an initial temperature of 20', function(){
-      expect(thermostat.temp).toEqual(20);
+  describe('temperature', function(){
+    describe('#DEFAULT_TEMP', function(){
+      it('should have an initial temperature of 20', function(){
+        expect(thermostat.DEFAULT_TEMP).toEqual(20);
+      });
+    });
+
+    describe('#currentTemp', function(){
+      it('should initialize as DEFAULT_TEMP', function(){
+        expect(thermostat.currentTemp).toEqual(thermostat.DEFAULT_TEMP);
+      });
+    });
+
+    describe('#MIN_TEMP', function() {
+      it('has a minimum temperature of 10', function(){
+        expect(thermostat.MIN_TEMP).toEqual(10);
+      });
+
+      it('does not allow temperature to fall below MIN_TEMP', function(){
+        for(i = 0; i < 10; i++) { thermostat.downButton(); }
+        error = 'Temperature cannot fall below 10';
+        expect(function() { thermostat.downButton(); }).toThrowError(error);
+      });
     });
   });
 
   describe('#upButton', function() {
-    it('should increase the temperature by 1', function() {
+    it('should increase the current temperature by 1', function() {
       thermostat.upButton();
-      expect(thermostat.temp).toEqual(21);
+      expect(thermostat.currentTemp).toEqual(21);
     });
   });
 
   describe('#downButton', function(){
-    it('should decrease the temperature by 1', function(){
+    it('should decrease the current temperature by 1', function(){
       thermostat.downButton();
-      expect(thermostat.temp).toEqual(19);
+      expect(thermostat.currentTemp).toEqual(19);
     });
   });
 
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,11 +1,15 @@
 var Thermostat = function(){
-  this.temp = 20;
+  this.DEFAULT_TEMP = 20;
+  this.MIN_TEMP = 10;
+  this.currentTemp = this.DEFAULT_TEMP;
 };
 
 Thermostat.prototype.upButton = function() {
-  return this.temp++;
+  return this.currentTemp++;
 };
 
 Thermostat.prototype.downButton = function() {
-  return this.temp--;
+  error = 'Temperature cannot fall below 10';
+  if (this.currentTemp === this.MIN_TEMP) throw new Error(error);
+  return this.currentTemp--;
 };
