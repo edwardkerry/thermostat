1ce3e79be1e9892323cfb910a2d8ac5f178ea353
edwardkerry <edwardkerry@gmail.com>
2016-01-06 16:05:02 +0000

    Add Max Temp and Power Save Mode

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -8,8 +8,20 @@ describe('Thermostat', function() {
 
   describe('temperature', function(){
     describe('#DEFAULT_TEMP', function(){
-      it('should have an initial temperature of 20', function(){
+      it('is 20', function(){
         expect(thermostat.DEFAULT_TEMP).toEqual(20);
+      });
+    });
+
+    describe('#MIN_TEMP', function() {
+      it('is 10', function(){
+        expect(thermostat.MIN_TEMP).toEqual(10);
+      });
+
+      it('does not allow temperature to fall below MIN_TEMP', function(){
+        for(i = 0; i < 10; i++) { thermostat.downButton(); }
+        error = 'Temperature cannot fall below 10';
+        expect(function() { thermostat.downButton(); }).toThrowError(error);
       });
     });
 
@@ -18,16 +30,19 @@ describe('Thermostat', function() {
         expect(thermostat.currentTemp).toEqual(thermostat.DEFAULT_TEMP);
       });
     });
+  });
 
-    describe('#MIN_TEMP', function() {
-      it('has a minimum temperature of 10', function(){
-        expect(thermostat.MIN_TEMP).toEqual(10);
-      });
+  describe('power save mode', function(){
+    it('should be on by default', function(){
+      expect(thermostat.powerSave).toEqual(true);
+    });
 
-      it('does not allow temperature to fall below MIN_TEMP', function(){
-        for(i = 0; i < 10; i++) { thermostat.downButton(); }
-        error = 'Temperature cannot fall below 10';
-        expect(function() { thermostat.downButton(); }).toThrowError(error);
+    describe('#powerSaveSWitch', function(){
+      it('turns power save on and off', function(){
+        thermostat.powerSaveSwitch();
+        expect(thermostat.powerSave).toEqual(false);
+        thermostat.powerSaveSwitch();
+        expect(thermostat.powerSave).toEqual(true);
       });
     });
   });
@@ -43,6 +58,19 @@ describe('Thermostat', function() {
     it('should decrease the current temperature by 1', function(){
       thermostat.downButton();
       expect(thermostat.currentTemp).toEqual(19);
+    });
+  });
+
+  describe('power save mode is on', function(){
+    it('should have a max temp of 25', function(){
+      expect(thermostat.MAX_TEMP).toEqual(25);
+    });
+  });
+
+  describe('power save mode is off', function(){
+    it('should have a max temp of 32', function(){
+      thermostat.powerSaveSwitch();
+      expect(thermostat.MAX_TEMP).toEqual(32);
     });
   });
 
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,15 +1,25 @@
 var Thermostat = function(){
   this.DEFAULT_TEMP = 20;
   this.MIN_TEMP = 10;
+  this.MAX_TEMP = 25;
   this.currentTemp = this.DEFAULT_TEMP;
+  this.powerSave = true;
 };
 
 Thermostat.prototype.upButton = function() {
-  return this.currentTemp++;
+   this.currentTemp++;
 };
 
 Thermostat.prototype.downButton = function() {
   error = 'Temperature cannot fall below 10';
   if (this.currentTemp === this.MIN_TEMP) throw new Error(error);
-  return this.currentTemp--;
+  this.currentTemp--;
+};
+
+Thermostat.prototype.powerSaveSwitch = function(){
+  this.powerSave = !this.powerSave;
+  if (this.powerSave)
+    { this.MAX_TEMP = 25;}
+  else
+   { this.MAX_TEMP = 32;}
 };
